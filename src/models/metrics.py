bddf56f15138b254117d8a9ae0b7c53ea37cf386
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-03-13 17:18:46 -0800

    First commit

---
+++ src/models/metrics.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def classification_metrics(predictions, actual):
+    """Return dictionary containing accuracy, precision, recall, and f1 score for 
+    classification tasks.
+
+    Args:
+        predictions (array): prediction values
+        actual (array): actual value
+
+    Returns:
+        [dictionary]: returns dictionary containing classification evaluation metrics.
+    """
+    accuracy= round(accuracy_score(actual, predictions),3)
+    precision= round(precision_score(actual, predictions),3)
+    recall= round(recall_score(actual, predictions),3)
+    f1_score= round(f1_score(actual, predictions),3)
+    scores= {'accuracy':accuracy,
+             'precision':precision,
+             'recall':recall,
+             'f1_score':f1_score
+            }
+    return scores
