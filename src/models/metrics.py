188f0ad99c5f2ad3b528e5b44a23902745191656
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-03-20 10:53:12 -0700

    Commiting previous changes from last work session

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+class Evaluation:
+    pass
 def classification_metrics(predictions, actual):
     """Return dictionary containing accuracy, precision, recall, and f1 score for 
     classification tasks.
@@ -14,10 +15,10 @@ def classification_metrics(predictions, actual):
     accuracy= round(accuracy_score(actual, predictions),3)
     precision= round(precision_score(actual, predictions),3)
     recall= round(recall_score(actual, predictions),3)
-    f1_score= round(f1_score(actual, predictions),3)
+    f1score= round(f1_score(actual, predictions),3)
     scores= {'accuracy':accuracy,
              'precision':precision,
              'recall':recall,
-             'f1_score':f1_score
+             'f1_score':f1score
             }
     return scores
