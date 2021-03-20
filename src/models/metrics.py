50ad2d64f306a9c0b3be4037c2578cbbdbaf3748
Kishen Sharma <kishen.r.sharma@gmail.com>
2021-03-20 12:54:51 -0700

    Refactored code, added Evaluation class and confusion matrix method

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -1,24 +1,32 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+
 class Evaluation:
-    pass
-def classification_metrics(predictions, actual):
-    """Return dictionary containing accuracy, precision, recall, and f1 score for 
-    classification tasks.
+    def __init__(self, predictions, actual):
+        self.predictions= predictions
+        self.actual= actual
+    
+    def classification_metrics(self):
+        """Return dictionary containing accuracy, precision, recall, and f1 score for 
+        classification tasks.
 
-    Args:
-        predictions (array): prediction values
-        actual (array): actual value
+        Args:
+            predictions (array): prediction values
+            actual (array): actual value
 
-    Returns:
-        [dictionary]: returns dictionary containing classification evaluation metrics.
-    """
-    accuracy= round(accuracy_score(actual, predictions),3)
-    precision= round(precision_score(actual, predictions),3)
-    recall= round(recall_score(actual, predictions),3)
-    f1score= round(f1_score(actual, predictions),3)
-    scores= {'accuracy':accuracy,
-             'precision':precision,
-             'recall':recall,
-             'f1_score':f1score
-            }
-    return scores
+        Returns:
+            [dictionary]: returns dictionary containing classification evaluation metrics.
+        """
+        accuracy= round(accuracy_score(self.actual, self.predictions),3)
+        precision= round(precision_score(self.actual, self.predictions),3)
+        recall= round(recall_score(self.actual, self.predictions),3)
+        f1score= round(f1_score(self.actual, self.predictions),3)
+        scores= {'accuracy':accuracy,
+                'precision':precision,
+                'recall':recall,
+                'f1_score':f1score
+                }
+        return scores
+
+    def confusion_matrix(self):
+        return confusion_matrix(self.actual, self.predictions)
